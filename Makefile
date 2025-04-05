7b57726ab4f626a0fc8a3b93e3076b5b18f6f395
pirotyyy <hiroki1423@docomo.ne.jp>
2025-04-05 18:54:35 +0900

    :heavy_plus_sign: dockerなどのセットアップ

---
+++ Makefile
@@ -0,0 +1,8 @@
+DOCKER_COMPOSE := docker compose
+
+up:
+	$(DOCKER_COMPOSE) up -d
+down:
+	$(DOCKER_COMPOSE) down
+build:
+	$(DOCKER_COMPOSE) build --no-cache
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
-package cmd
+package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	e := echo.New()
+
+	// health check
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
