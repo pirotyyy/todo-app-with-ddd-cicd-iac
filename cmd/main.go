f02d06b829fee659a884e827b7f56f8dcad6d6a0
pirotyyy <hiroki1423@docomo.ne.jp>
2025-04-05 21:33:50 +0900

    :sparkles: confの読み込みとloggerの初期化

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"context"
+	"log/slog"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/pirotyyy/todo-app-with-ddd-cicd-iac/internal/config"
+	"github.com/pirotyyy/todo-app-with-ddd-cicd-iac/internal/logger"
 )
 
 func main() {
+	ctx := context.Background() // TODO: ちゃんとやる
+
+	conf, err := config.Load()
+	if err != nil {
+		logger.Error(ctx, err.Error())
+	}
+	slog.SetDefault(slog.New(logger.NewHandler(conf.Log)))
+
 	e := echo.New()
+
+	e.Use(middleware.Recover())
 
 	// health check
 	e.GET("/", func(c echo.Context) error {
